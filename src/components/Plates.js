1a5e319296b3b9f1b25cfd0d760b7f4eddd88a87
riveracangela@gmail.com <riveracangela@gmail.com>
2017-09-09 18:57:00 -0700

    Render plates in Plates component

--- src/components/Plates.js
+++ src/components/Plates.js
@@ -8,29 +8,10 @@ class Plates extends Component {
     }
   }
 
-  componentWillMount() {
-        const kdTree = document.createElement("script");
-        const jspolygon = document.createElement("script");
-        const ishihara = document.createElement("script");
-
-        kdTree.src = "kdTree.js"
-        jspolygon.src = "jspolygon.js"
-        ishihara.src = "ishihara.js"
-        
-        kdTree.async = true;
-        jspolygon.async = true;
-        ishihara.async = true;
-
-        document.body.appendChild(kdTree);
-        document.body.appendChild(jspolygon);
-        document.body.appendChild(ishihara);
-    },
-
   render(){
     return (
       <div>
       <p>Questions Component</p>
-      <img src={this.state.plateImageSrc} />
       <canvas id="canvas">Your browser does not support canvas.</canvas>
       </div>
       );
