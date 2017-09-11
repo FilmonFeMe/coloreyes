8821ecbb690d3658c8f144038be62d7d26e13b2d
riveracangela@gmail.com <riveracangela@gmail.com>
2017-09-11 15:49:38 -0700

    Add homepage image, add content for instructions, about us and results page

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,7 +4,7 @@ class Result extends Component {
   render() {
     return(
       <div htmlClass="results">
-        <div>Quiz completed.</div>
+        <p>The answers provided indicate a difficulty interpreting red and green colors.  Red and/or green may appear as one color, or as grey.  Colors which contain hues of red or green may also be impacted.  For example, purple is a mixture of both red and blue; therefore, purple may appear indistinguishable from blue to someone affected by red-green colorblindness.</p>
         <div>Correct answers: {this.props.numCorrect}/{this.props.numberOfQuestions}</div>
       </div>
       );
