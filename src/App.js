c453f91ed157d73c82f651ab42df2c7811668e97
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-09-13 15:57:09 -0700

    Add scorebar to Ishihara's results.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Quiz from './components/Quiz.js';
 
+// numberOfQuestions should be 19.
+
 class App extends Component {
   render() {
     return (
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import SocialMedia from './SocialMedia.js'
+import ScoreBar from './ScoreBar.js'
 
 class Result extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      score: (this.props.numCorrect - 1)/(this.props.numberOfQuestions - 1)
+    };
   }
 
   render() {
@@ -12,18 +17,23 @@ class Result extends Component {
     if (this.props.numCorrect == 0){
       return(
         <div className="results">
+          <div id="score">
+            <ScoreBar score={this.state.score}/>
+          </div>
           <p>The answer provided indicates a difficulty viewing and/or interpreting the illustration.</p>
           <p><strong>Disclaimer:</strong> The information provided by Filmon Mengisteab is not a substitute for professional medical advice, diagnosis, or treatment; it is intended only for general informational purposes.   If you have any concerns or questions about your health, you must consult with your physician or health-care professional.</p>
           <SocialMedia message="The answer provided indicates a difficulty viewing and/or interpreting the illustration."/>
         </div>
       );
     }
-    else if ((this.props.numCorrect / this.props.numberOfQuestions) < .90) {
+    else if (this.score < .90) {
       return(
         <div className="results">
+          <div id="score">
+            <ScoreBar score={this.state.score}/>
+          </div>
           <p>The answers provided indicate a difficulty interpreting red and green colors.  Red and/or green may appear as one color, or as grey.  Colors which contain hues of red or green may also be impacted.  For example, purple is a mixture of both red and blue; therefore, purple may appear indistinguishable from blue to someone affected by red-green colorblindness.</p>
           <p><strong>Disclaimer:</strong> The information provided by Filmon Mengisteab is not a substitute for professional medical advice, diagnosis, or treatment; it is intended only for general informational purposes.   If you have any concerns or questions about your health, you must consult with your physician or health-care professional.</p>
-          <div>Correct answers: {this.props.numCorrect}/{this.props.numberOfQuestions}</div>
           <SocialMedia message="The answers provided indicate a difficulty interpreting red and green colors."/>
         </div>
       );
@@ -31,6 +41,9 @@ class Result extends Component {
     else {
       return(
         <div className="results">
+          <div id="score">
+            <ScoreBar score={this.state.score}/>
+          </div>
           <p>The answers provided indicate an normal ability to distinguish between red and green colors.</p>
           <p><strong>Disclaimer:</strong> The information provided by Filmon Mengisteab is not a substitute for professional medical advice, diagnosis, or treatment; it is intended only for general informational purposes.   If you have any concerns or questions about your health, you must consult with your physician or health-care professional.</p>
           <SocialMedia message="The answers provided indicate an normal ability to distinguish between red and green colors."/>
