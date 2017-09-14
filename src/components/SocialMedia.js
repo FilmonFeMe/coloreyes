47c3cd90e98c471c6b5ffd0fcc4429dc472b6572
Gloria A. Cruz <email": null>
2017-09-13 19:49:22 -0700

    Implement colorblind view simulator.

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -9,7 +9,8 @@ class SocialMedia extends Component {
   render(){
     return (
       <div className="share">
-        <div class="fb-share-button" data-href="http://www.iseergb.com/" data-layout="button" data-size="large" data-mobile-iframe="false"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.iseergb.com%2F&amp;src=sdkpreparse"><img className="fb-share" src="images/fb_share.jpeg" alt="Share"></img></a></div>
+        <div class="fb-share-button" data-href="http://www.iseergb.com/" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.iseergb.com%2F&amp;src=sdkpreparse"><img className="fb-share" src="images/fb_share.jpeg" alt="Share"></img></a></div>
+        <div class="fb-quote"></div>
         <a class="twitter-share-button" href={"https://twitter.com/intent/tweet?text=" + this.props.message + "&hashtags=coloreyes,colorblindtest&url=http://www.iseergb.com/"} data-size="large">Tweet</a>
       </div>
     );
