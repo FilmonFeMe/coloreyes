848383cc66617b6edaa5ea336bc501a1e49d16a2
Nestor Ceron <nestor_ceron@yahoo.com>
2017-09-15 14:58:29 -0700

    Add about page content

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -7,7 +7,7 @@ class AboutUs extends Component {
       <div id="aboutUs">
         <h1>About Us</h1>
         <p>ColorEyes is a non-profit company founded by a group of Web Developers in San Francisco, California.</p>
-        <p>Our goal is to connect the color-blind community around the Bay Area and provide simple and efficient color perception tests.</p>
+        <p>Our goal is to connect the color-blind community around the Bay Area and provide simple and efficient color perception tests. Driven by empathy, ColorEyes aims to move beyond color vision testing but also provide a community forumn online where people can have like-minded conversations and share resources. ColorEyes is the first ever application that combines two world class color vision tests, the Ishihara and Munsell test.</p>
       </div>
     );
   }
